89fe2da24b03b5696ac9d74a4f32fb430172be4f
Darlington720 <darlingtonakampa@gmail.com>
2022-06-22 17:18:19 +0300

    new file:   nkumbaServer.js 	new file:   package-lock.json 	new file:   package.json 	new file:   public/assets/adaptive-icon.png 	new file:   public/assets/apple.png 	new file:   public/assets/background.jpg 	new file:   public/assets/chair.jpg 	new file:   public/assets/couch.jpg 	new file:   public/assets/eddy.jpg 	new file:   public/assets/face.jpg 	new file:   public/assets/favicon.png 	new file:   public/assets/furniture1.jpg 	new file:   public/assets/icon.png 	new file:   public/assets/iphone.jpg 	new file:   public/assets/iphone2.jpg 	new file:   public/assets/jacket.jpg 	new file:   public/assets/logo-red.png 	new file:   public/assets/mac2.jpg 	new file:   public/assets/macbook.jpg 	new file:   public/assets/mosh.jpg 	new file:   public/assets/person.jpeg 	new file:   public/assets/peter.jpg 	new file:   public/assets/room.jpg 	new file:   public/assets/splash.png 	new file:   public/assets/usedmac.jpg

---
+++ nkumbaServer.js
@@ -0,0 +1,378 @@
+const express = require("express");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const knex = require("knex");
+const { response } = require("express");
+const cors = require("cors");
+const { cookie } = require("express/lib/response");
+const req = require("express/lib/request");
+
+const upload = multer();
+const app = express();
+const baseIp = "10.42.0.1";
+const secret = "mySecret";
+const port = 9001;
+
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+app.use(upload.any());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+
+const database = knex({
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "nkumba",
+  },
+});
+
+const data = [
+  "students",
+  "students_signin_book",
+  "visitors",
+  "non_teaching_staff",
+];
+
+data.map((item) =>
+  app.get(`/${item}`, (req, res) => {
+    database
+      .orderBy("id")
+      .select("*")
+      .from(item)
+      .then((data) => {
+        res.send(data);
+      });
+  })
+);
+
+app.get("/visitorData", (req, res) => {
+  const d = new Date();
+  const date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+
+  database("visitors")
+    .join(
+      "users",
+      "visitors.signed_in_by",
+
+      "=",
+      "users.id"
+    )
+    .where("visitors.date", "=", date)
+    .orderBy("time", "desc")
+    .select("*")
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+app.get("/myVisitors/:user_id", (req, res) => {
+  const { user_id } = req.params;
+  console.log(user_id);
+  const d = new Date();
+  const date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+
+  database("visitors")
+    .join(
+      "users",
+      "visitors.signed_in_by",
+
+      "=",
+      "users.id"
+    )
+    .where("visitors.date", "=", date)
+    .select("*")
+    .where({
+      signed_in_by: user_id,
+      date: date,
+    })
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+app.get("/myStudents/:student_id", (req, res) => {
+  const { student_id } = req.params;
+  console.log(student_id);
+  const d = new Date();
+  const date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+
+  database("students_signin_book")
+    .join(
+      "users",
+      "students_signin_book.signed_in_by",
+
+      "=",
+      "users.id"
+    )
+    .where("students_signin_book.signin_date", "=", date)
+    .select("*")
+    .where({
+      signed_in_by: student_id,
+      signin_date: date,
+    })
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+app.get("/studentData", (req, res) => {
+  const d = new Date();
+  const date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+
+  database("students")
+    .join(
+      "students_signin_book",
+      "students.stu_id",
+
+      "=",
+      "students_signin_book.stu_id"
+    )
+
+    .join(
+      "students_signout_book",
+      "students_signin_book.signin_date",
+      "=",
+      "students_signout_book.signin_date"
+    )
+    .where("students_signin_book.signin_date", "=", date)
+
+    .select("*")
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+app.get("/student/:studentNo", (req, res) => {
+  const { studentNo } = req.params;
+  console.log(studentNo);
+  const d = new Date();
+  const date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+
+  database
+    .select("*")
+    .from("students_signin_book")
+    .where({
+      stu_id: studentNo,
+      signin_date: date,
+    })
+    .then((data) => {
+      // res.send(data);
+      if (data.length > 0) {
+        database
+          .select("*")
+          .from("students")
+
+          .join(
+            "students_signin_book",
+            "students.stu_id",
+            "=",
+            "students_signin_book.stu_id"
+          )
+
+          .join(
+            "students_signout_book",
+            "students.stu_id",
+            "=",
+            "students_signout_book.stu_id"
+          )
+          .where("students.stu_id", "=", studentNo)
+          .andWhere("students_signout_book.signin_date", "=", date)
+          .then((data3) => {
+            // res.send(data3);
+            if (data3[0].sign_out !== null) {
+              res.send("Already registered");
+            } else {
+              res.send([
+                data3[0],
+                {
+                  todaysStatus: true,
+                  imageUrl: `http://${baseIp}:${port}/assets/${data3[0].image}`,
+                },
+              ]);
+            }
+          });
+      } else {
+        database
+          .select("*")
+          .from("students")
+          .where({
+            stu_id: studentNo,
+          })
+          .then((data2) => {
+            res.send([
+              ...data2,
+              {
+                todaysStatus: false,
+                imageUrl: data2[0]
+                  ? `http://${baseIp}:${port}/assets/${data2[0].image}`
+                  : "http://${baseIp}:${port}/assets/jacket.jpg",
+              },
+            ]);
+          });
+      }
+    });
+
+  // database("students")
+  //   .join(
+  //     "students_signin_book",
+  //     "students.stu_id",
+  //     "=",
+  //     "students_signin_book.stu_id"
+  //   )
+  //   .select("*")
+  //   // .where("quantity", ">", 0)
+
+  //   .then((data) => {
+  //     res.send(data);
+  //   });
+});
+
+/**
+ * You first need to create a formatting function to pad numbers to two digits…
+ **/
+function twoDigits(d) {
+  if (0 <= d && d < 10) return "0" + d.toString();
+  if (-10 < d && d < 0) return "-0" + (-1 * d).toString();
+  return d.toString();
+}
+
+/**
+ * …and then create the method to output the date string as desired.
+ * Some people hate using prototypes this way, but if you are going
+ * to apply this to more than one Date object, having it as a prototype
+ * makes sense.
+ **/
+Date.prototype.toMysqlFormat = function () {
+  return (
+    this.getUTCFullYear() +
+    "-" +
+    twoDigits(1 + this.getUTCMonth()) +
+    "-" +
+    twoDigits(this.getUTCDate()) +
+    " " +
+    twoDigits(this.getUTCHours()) +
+    ":" +
+    twoDigits(this.getUTCMinutes()) +
+    ":" +
+    twoDigits(this.getUTCSeconds())
+  );
+};
+
+app.post("/studentReg", (req, res) => {
+  const { stu_id, temp, signed_in_by } = req.body;
+  console.log(req.body);
+  const d = new Date();
+  const date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+  const time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+  console.log(
+    "time",
+    d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+  );
+
+  database("students_signin_book")
+    .insert({
+      stu_id: stu_id,
+      temperature: temp,
+      signin_date: date,
+      signin_time: time,
+      signed_in_by,
+    })
+    .then((data) => {
+      database("students_signout_book")
+        .insert({
+          stu_id: stu_id,
+          signin_date: date,
+        })
+        .then((data2) => {
+          res.send("Received the data");
+        });
+    })
+    .catch((err) => res.send(err));
+});
+
+app.post("/studentSignout/:studentNo", (req, res) => {
+  const { studentNo } = req.params;
+  console.log(studentNo);
+  const d = new Date();
+  const date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+  const time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+  database("students_signout_book")
+    // .where("stu_id", "=", studentNo)
+    .where(function () {
+      this.where("stu_id", "=", studentNo);
+    })
+    .andWhere(function () {
+      this.where("signin_date", "=", date);
+    })
+    .update({
+      sign_out: new Date().toMysqlFormat(),
+      signout_date: date,
+      signout_time: time,
+    })
+    .then((data) => {
+      res.send("received timestamop");
+    });
+});
+
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  database
+    .select("*")
+    .where({
+      username: username,
+      password: password,
+    })
+    .from("users")
+    .then((user) => {
+      console.log(user);
+      if (!user[0]) {
+        return res.status(400).json({ error: "Invalid email or password " });
+      } else {
+        return res.send({
+          ...user[0],
+          imageUrl: `http://${baseIp}:${port}/assets/${user[0].image}`,
+        });
+      }
+      // const token = jwt.sign(
+      //   {
+      //     id: user[0].id,
+      //     email: user[0].email,
+      //     name: user[0].username,
+      //     address: user[0].address,
+      //     fullName: user[0].fullname,
+      //     image: user[0].image,
+      //   },
+      //   secret
+      // );
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: "Invalid email or password" });
+    });
+});
+
+app.post("/api/addVisitor", (req, res) => {
+  const { full_name, reason, office, signed_in_by } = req.body;
+  console.log(req.body);
+  database("visitors")
+    .insert({
+      v_full_name: full_name,
+      reason,
+      office,
+      signed_in_by,
+    })
+    .then((data) => res.status(200).send("Received the data"))
+    .catch((err) => res.status(400).send("Failed to send the data " + err));
+});
+
+app.listen(port, baseIp, () => console.log(`App is running on port ${port}`));
